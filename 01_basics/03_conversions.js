e67a72420088fdc3d148b58b3481476b39912d30
shashankshekhar-11 <121714563+shashankshekhar-11@users.noreply.github.com>
2024-03-15 18:13:36 +0000

    minor changes

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -46,10 +46,10 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); //=>12
+// console.log(1 + "2"); //=>12
+// console.log("1" + 2 + 2); //=>122
+ //console.log(1 + 2 + "2"); //=>32
 
 // console.log( (3 + 4) * 5 % 3);
 
@@ -62,6 +62,8 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 ++gameCounter;
+console.log(gameCounter);
+gameCounter++;
 console.log(gameCounter);
 
 // link to study
