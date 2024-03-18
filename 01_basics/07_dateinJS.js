4d1caa752fe53c85bb563e49babc8f9e5948cc07
shashankshekhar-11 <121714563+shashankshekhar-11@users.noreply.github.com>
2024-03-18 17:47:07 +0000

    dates

---
+++ 01_basics/07_dateinJS.js
@@ -0,0 +1,10 @@
+// Dates
+
+let myDates = new Date();
+console.log(myDates.toDateString());
+console.log(myDates.toLocaleDateString());
+
+
+
+let myCreatedDate = new Date(2024,0,23)
+console.log(myCreatedDate.toDateString());
