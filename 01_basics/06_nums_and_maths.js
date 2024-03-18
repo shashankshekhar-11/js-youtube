e6edc00bac800568d0945ed722cea1e4e99a07eb
shashankshekhar-11 <121714563+shashankshekhar-11@users.noreply.github.com>
2024-03-18 17:38:13 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+/*
+const score=400;
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+
+
+const otherNumber=23.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds =10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+*/
+
+//+++++++++++++++++Maths++++++++++++++++++++++++++++++++////
+//console.log(Math.abs(-7));
+console.log(Math.random()); //=> random value b/w 0-1
+console.log(Math.floor(Math.random()*10+1));
+
+
+const min=1
+const max= 6;
+
+console.log(Math.floor(Math.random() *(max- min +1)))
+console.log(Math.floor(Math.random() *(max- min +1)))
+console.log(Math.floor(Math.random() *(max- min +1)))
